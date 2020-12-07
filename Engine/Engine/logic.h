4f38f6a4af7271bb67679561e38d405fd08fa08c
Kai-Builder <gkai@gmail.com>
2020-12-06 19:43:14 -0500

    Update logic.h

--- Engine/Engine/logic.h
+++ Engine/Engine/logic.h
@@ -4,7 +4,14 @@ using namespace std;
 #ifndef GRAVITY
 #endif NULL	
 vector <int> BasicGravityIntegers{100, 200, 990, 456, 3500}
-
-
-
-class MainWorld
+TransParentObjects ne;
+class MainWorld {
+	int AddWorldGravityInteger(int IntegerType) {
+		BasicGravityIntegers.push_back(IntegerType);
+		return 0;
+	}
+	int AddWorldBaseTickSpeeds(int TickTypeWorld) {
+		ne.push_back(TickTypeWorld)
+			return 0;
+	}
+};
